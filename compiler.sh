7869eee97bf192c372bf862977a414b37ecaf86f
YingchangLiu <58721349+YingchangLiu@users.noreply.github.com>
2024-05-22 10:37:38 +0800

    update compiler.sh

--- compiler.sh
+++ compiler.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./configure CC=gcc-11 CXX=g++-11 CUDAFLAGS='-x cu --compiler-bindir=/usr/x86_64-pc-linux-gnu/gcc-bin/11/ -ccbin=/usr/bin/g++-11' API=c++,f90
+./configure CC=gcc-11 CXX=g++-11 F77=gfortran-11 FORTRAN=gfortran-11 F90=gfortran-11 CUDAFLAGS='-x cu --compiler-bindir=/usr/x86_64-pc-linux-gnu/gcc-bin/11/ -ccbin=/usr/bin/g++-11' API=c++,f90
 
