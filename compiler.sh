7f752bc14980ae61094df7f377e697ae8ef3e02c
kelen <58721349+YingchangLiu@users.noreply.github.com>
2025-03-04 20:54:00 +0800

    update compiler.sh

--- compiler.sh
+++ compiler.sh
@@ -1,3 +1,33 @@
-#!/bin/bash
-./configure CC=gcc-11 CXX=g++-11 F77=gfortran-11 FORTRAN=gfortran-11 F90=gfortran-11 CUDAFLAGS='-x cu --compiler-bindir=/usr/x86_64-pc-linux-gnu/gcc-bin/11/ -ccbin=/usr/bin/g++-11' API=c++,f90
+#!/usr/bin/env bash
 
+# This script provides an example for configuring and compiling Madagascar.
+# It is intended for use only if issues arise during the standard compilation process.
+# Please modify this script to suit your specific distribution and environment.
+
+## C++ and Fortran-90 API bindings
+API=c++,f90
+
+## Installation prefix
+prefix=$HOME/opt/ahay
+
+## The default compilers for C, C++, and Fortran
+CC=gcc-11
+CXX=g++-11
+F77=gfortran-11
+FORTRAN=gfortran-11
+F90=gfortran-11
+
+## Specify the CUDA flags
+CUDAFLAGS='-x cu --compiler-bindir=/usr/x86_64-pc-linux-gnu/gcc-bin/11/ -ccbin=/usr/bin/g++-11'
+
+
+
+## Configure madagascar
+./configure API=$API --prefix=$prefix CC=$CC CXX=$CXX F77=$F77 FORTRAN=$FORTRAN F90=$F90 CUDAFLAGS=$CUDAFLAGS
+## Full configuration command for reference
+## ./configure CC=gcc-11 CXX=g++-11 F77=gfortran-11 FORTRAN=gfortran-11 F90=gfortran-11 CUDAFLAGS='-x cu --compiler-bindir=/usr/x86_64-pc-linux-gnu/gcc-bin/11/ -ccbin=/usr/bin/g++-11' API=c++,f90
+
+
+## Compile madagascar
+# make
+# make install
